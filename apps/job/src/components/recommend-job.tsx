ee87dbefd1ae21ac2d0b5c7ebb115ffd274ea5b3
5undaye <5undaye@github.com>
2025-03-02 01:57:29 +0900

    feat: 추천 채용 공고 프레그먼트 작성

---
+++ apps/job/src/components/recommend-job.tsx
@@ -0,0 +1,25 @@
+import { RecommendJobWrapper } from "./recommend-job.styles";
+import { useShellNavigate } from "@career-up/shell-router";
+
+import "./recommend-job.styles";
+
+interface RecommendJobProps {
+  id: number;
+  position: string;
+  company: string;
+}
+
+const RecommendJob = ({ id, position, company }: RecommendJobProps) => {
+  const navigate = useShellNavigate();
+  const onClick = () => {
+    navigate(`/job/${id}`);
+  };
+  return (
+    <RecommendJobWrapper onClick={onClick}>
+      <div className="job--recommend-job-position">{position}</div>
+      <div className="job--recommend-job-company">{company}</div>
+    </RecommendJobWrapper>
+  );
+};
+
+export default RecommendJob;
